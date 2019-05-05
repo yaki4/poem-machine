388efd5ba667e67d900dcee0a9a5391d9731ffa9
gabriel <gabriel.corbel@gmail.com>
2019-05-05 19:47:08 -0400

    add all

---
+++ components/terminal.vue
@@ -0,0 +1,35 @@
+<script>
+let VueTerminal = {}
+if (process.browser) {
+  VueTerminal = require('vue-terminal-ui')
+}
+export default {
+  components: {
+    VueTerminal
+  },
+  methods: {
+    onCommand(data, resolve, reject) {
+      /* eslint-disable */
+      console.log('CO', data)
+    }
+  }
+}
+</script>
+<template lang="pug">
+.terminal
+    VueTerminal(intro="bnoj", console-sign="$", allow-arbitrary, height="500px", @command='onCommand')
+</template>
+<style>
+.index{
+  background-color: #0000FF !important;
+  font-family: 'IBM Plex Sans', sans-serif;
+  font-size: 2em;
+}
+.command{
+  font-family: 'IBM Plex Sans', sans-serif;
+  font-size: 2em;
+}
+.startcommand{
+  display: flex;
+}
+</style>
---
+++ components/testComp.vue
@@ -0,0 +1,16 @@
+<script>
+
+export default {
+  components: {
+  },
+  methods: {
+  }
+}
+</script>
+<template lang="pug">
+.test-comp
+    h1 BONJOUR
+</template>
+<style>
+
+</style>
